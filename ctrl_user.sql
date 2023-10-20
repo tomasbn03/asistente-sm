3459c7548bedc89a5137cfb9201ab339975d976f
tomasbn03 <tomasbonilla32@gmail.com>
2023-10-20 15:10:29 -0600

    Se agrega el archivo de la base de datos

--- ctrl_user.sql
+++ ctrl_user.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-10-2023 a las 01:26:29
+-- Tiempo de generación: 20-10-2023 a las 23:08:53
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.0.28
 
@@ -32,19 +32,22 @@ CREATE TABLE `usuarios` (
   `nombre` varchar(200) NOT NULL,
   `usuario` varchar(200) NOT NULL,
   `correo` varchar(400) NOT NULL,
-  `contrasenia` varchar(200) DEFAULT NULL
+  `contrasenia` varchar(200) DEFAULT NULL,
+  `telefono` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nombre`, `usuario`, `correo`, `contrasenia`) VALUES
-(19, 'Angel Martinez', 'angelm', 'angelm@gmail.com', '2003'),
-(20, 'Tomas Bonilla', 'tomasbnll', 'tomasbonilla@gmail.com', 'admin'),
-(21, 'Jordan', 'Jordan1', 'jordan.molina22@itca.edu.sv', '12345'),
-(23, 'Cristiano Ronaldo', 'cr7', 'elbicho@cr7.com', 'siu'),
-(24, 'Leonel Andrés', 'messi', 'leomessi@adidas.org', 'goat');
+INSERT INTO `usuarios` (`id`, `nombre`, `usuario`, `correo`, `contrasenia`, `telefono`) VALUES
+(19, 'Angel Martinez', 'angelm', 'angelm@gmail.com', '2003', '79322840'),
+(20, 'Enrique Bonilla', 'tomasbnll', 'tomas@gmail.com', 'root', '76576555'),
+(21, 'Jordan', 'Jordan1', 'jordan.molina22@itca.edu.sv', '12345', '76654114'),
+(23, 'Cristiano Ronaldo', 'cr7', 'elbicho@cr7.com', 'siu', '60655786'),
+(24, 'Leonel Andrés', 'messi', 'leomessi@adidas.org', 'goat', '60005090'),
+(27, 'Juan Hernandez', 'jhernandez', 'jhernandez@gmail.com', 'games', '78876541'),
+(28, 'Fernando Páz', 'ferpz', 'fernandopz@gmail.com', 'secreto', '60009909');
 
 --
 -- Índices para tablas volcadas
@@ -64,7 +67,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
