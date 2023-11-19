34f8a5261df622d3ed9561f7f80ed302d93ca2c4
tomasbn03 <tomasbonilla32@gmail.com>
2023-11-19 12:00:33 -0600

    Astronauta

---
+++ assets/js/login-register.js
@@ -0,0 +1,19 @@
+// Funcionamiento para el formulario de inicio de sesion y registro
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+const isSignUpActive = container.classList.contains("right-panel-active");
+
+signUpButton.addEventListener('click', () => {
+    container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+    container.classList.remove("right-panel-active");
+});
+
+
+if (isSignUpActive) {
+    container.classList.add("right-panel-active");
+}
